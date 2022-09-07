d8e7793df536f02825b439f1c105c1e0187e8f56
Beatriz Alencar Kuriki <b.f.kuriki@gmail.com>
2022-09-07 18:06:56 -0300

    feat:Backend and Frontend projects created

---
+++ backend/src/main/java/com/devsuperior/dkmeta/DkmetaApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dkmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DkmetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DkmetaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dkmeta/DkmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dkmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DkmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
