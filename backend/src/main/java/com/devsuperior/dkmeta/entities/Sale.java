63f6f39505d6bf1ae53cba4d64b6dc5d15cbfd0a
Beatriz Alencar Kuriki <b.f.kuriki@gmail.com>
2022-09-09 11:33:05 -0300

    feat: date select

--- backend/src/main/java/com/devsuperior/dkmeta/entities/Sale.java
+++ backend/src/main/java/com/devsuperior/dkmeta/entities/Sale.java
@@ -10,7 +10,7 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_sales")
-public class Vendas {
+public class Sale {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,7 +21,7 @@ public class Vendas {
 	private Double amount;
 	private LocalDate date;
 	
-	public Vendas() {
+	public Sale() {
 		
 	}
 
